1af98253edec7724f1ef34c1182f65af0c613f29
John <cooks.johns@gmail.com>
2018-05-22 15:49:02 -0500

    basic statements

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,9 +6,9 @@ CREATE TABLE IF NOT EXISTS albums (
 
   id INT unsigned NOT NULL AUTO_INCREMENT,
   artist VARCHAR (50),
-  name VARCHAR (50),
+  name VARCHAR (55),
   release_date INT(8),
-  sales DECIMAL (3, 2),
+  sales INT(8),
   genre VARCHAR (50),
   PRIMARY KEY (id)
 )
--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,8 +1,9 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
 USE codeup_test_db;
 
 INSERT INTO albums (artist, name, release_date, genre, sales)
-VALUES
-('Michael Jackson', 'Thriller', 1982,   'Pop, Rock, R&B',   47.3),
+VALUES('Michael Jackson', 'Thriller', 1982,   'Pop, Rock, R&B',   47.3),
 ('AC/DC','Back in Black', 1980, 'Hard rock', 26.1),
 ('Pink Floyd',  'The Dark Side of the Moon',    1973,   'Progressive rock', 24.2),
 ('Meat Loaf',   'Bat Out of Hell',  1977,   'Hard rock, Progressive rock',  21.5),
@@ -31,5 +32,5 @@ VALUES
 ('Nirvana', 'Nevermind',    1991,   'Grunge, Alternative rock', 16.7),
 ('Pink Floyd',  'The Wall', 1979,   'Progressive rock', 17.6),
 ('Santana', 'Supernatural', 1999,   'Rock', 20.5),
-('Guns N'' Roses',  'Appetite for Destruction', 1987,   'Hard rock',    21.6),
+('Guns N'' Roses',  'Appetite for Destruction', 1987,   'Hard rock',    21.6);
 
