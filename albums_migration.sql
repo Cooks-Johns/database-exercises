45c7f5eee24a107e09d4350e7586ecfa4144801e
John <cooks.johns@gmail.com>
2018-05-22 11:28:38 -0500

    tables

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+
+  id INT unsigned NOT NULL AUTO_INCREMENT,
+  artist VARCHAR(50),
+  name VARCHAR(50),
+  release_date INT(8),
+  sales BIGINT(10),
+  genre VARCHAR(50),
+  PRIMARY KEY (id)
+)
