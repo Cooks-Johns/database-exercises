00dcff8d9435a0d6f9aa63bde7a70f922c163123
John <cooks.johns@gmail.com>
2018-05-23 15:39:47 -0500

    basic statements

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,37 +4,40 @@ USE employees;
 --2
 SELECT *
 FROM employees
-WHERE last_name
-In (
-  -- type in last names 'name', 'name'
-  'Irena', 'Vidya', 'Maya'
-
-);
-
+WHERE first_name In ('Irena', 'Vidya', 'Maya');
+-- --
 
 --3
 
 SELECT *
 FROM employees
-WHERE last_name
-IN (
-  'E%'
-);
+WHERE last_name LIKE 'E%';
 
 
 --4
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN 1990 AND 1999;
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE  hire_date like "199%";
 
 
 
 --5
 
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
 
 
 
 --6
+
+SELECT *
+FROM employees
+WHERE last_name like '%q%';
 
 
 
@@ -43,7 +46,19 @@ WHERE hire_date BETWEEN 1990 AND 1999;
 
 -- 1
 
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena'
+OR first_name - 'Vidya'
+OR first_name = 'Maya')
+AND  gender = 'M';
+
+
 ---2
+SELECT *
+FROM emp
+
+
 
 
 ---3
