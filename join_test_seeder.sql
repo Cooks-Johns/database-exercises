5c7921f3291428bb8547e29d1fb3dbf32c469453
John <cooks.johns@gmail.com>
2018-05-28 15:05:29 -0500

    join Example db

---
+++ join_test_seeder.sql
@@ -0,0 +1,53 @@
+USE join_test_db;
+
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+
+CREATE TABLE roles (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  role_id INT UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+
+CREATE TABLE posts (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255),
+  user_id INT UNSIGNED,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id) VALUES
+  (''bob'', ''bob@example.com'', 1),
+  (''joe'', ''joe@example.com'', 2),
+  (''sally'', ''sally@example.com'', 3),
+  (''adam'', ''adam@example.com'', NULL),
+  (''jane'', ''jane@example.com'', 2),
+  (''mike'', ''mike@example.com'', 1);
+
+INSERT INTO posts (title, user_id) VALUES
+  (''Hello World'', 3),
+  (''Goodbye World'', NULL),
+  (''JS Rocks ZOMG JS'', 2),
+  (''Jquery is pretty ok...'', 2),
+  (''npm is trickier...'', 6),
+  (''Java is my favorite!!!'', 3),
+  (''IntelliJ is A-OK'', 5);
+
