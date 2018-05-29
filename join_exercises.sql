055aa2ab18c926a425b8f90f1fc9395cde12a630
John <cooks.johns@gmail.com>
2018-05-29 15:23:54 -0500

    finishing up sub_queries/ relationships / mysql now working on the bonus.

--- join_exercises.sql
+++ join_exercises.sql
@@ -64,6 +64,8 @@ JOIN departments as d
  On d.dept_no = em.emp_no;
 
 
+
+
 -- BONUS
 
 
@@ -81,6 +83,42 @@ UNION SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name
 FROM dept_manager as dm
 RIGHT JOIN employees as e
   ON e.emp_no = dm.emp_no;
+
+
+
+
+-- Here’s the bonus for the JOIN exercise:
+--
+-- Use joins along with any MySQL statement
+-- we’ve covered so far to answer the following questions:
+--   1. Show current average salaries for all
+--       employees, grouped by gender, to
+--       show if the average is higher for
+--       male/female salaries.
+
+
+
+
+--   2. Show historic average salaries for all
+--       employees, grouped by gender, to
+--       show if the average is higher for
+--       male/female salaries.
+
+
+--
+--   3. Show current average salaries of all
+--    managers, grouped by gender, to
+--     answer if the company pays average
+--     manager salaries higher or lower based on gender.
+
+
+
+--
+--   4. Show historic average salaries of all
+--       managers, grouped by gender, to
+--       answer if the company pays average
+--       manager salaries higher
+--       or lower based on gender.
 
 
 
@@ -129,3 +167,6 @@ SELECT *
 FROM salaries
 JOIN employees ON salaries.emp_no = employees.emp_no
 WHERE salaries.to_date > now();
+
+
+
--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,14 +1,102 @@
 
+-- 1
+
+SELECT *
+  FROM employees
+  WHERE hire_date = '1990-10-22';
+
+
+-- 2
+
+
+
+ SELECT title,
+ FROM employees e
+ JOIN titles t
+  On e.emp_no = t.emp_no
+ WHERE emp_no =(
+  SELECT first_name, last_name
+  FROM employees e2
+  WHERE e2.first_name = 'Aamod'
+
+ );
+
+
+ SELECT first_name, last_name
+  FROM employees
+  WHERE emp_no =(
+  SELECT emp_no
+  FROM dept_manager
+  WHERE to_date > curdate()
+  AND dept_no = (
+    SELECT dept_no
+    FROM departments
+    WHERE dept_name = 'Customer Service'
+  )
+);
+
+--  3
+
+
+SELECT first_name, last_name
+FROM employees e
+JOIN dept_manager dm
+  ON dm.emp_no = e.emp_no
+  WHERE e.gender = 'F' AND dm.to_date >= curdate();
+
+
+-- BONUS
+--
+-- Find all the department names that currently have female managers.
+-- department names
+-- female managers
+
+ SELECT dept_name
+  FROM departments
+  WHERE emp_no =(
+  SELECT emp_no
+  FROM dept_manager
+  WHERE to_date > curdate()
+  AND dept_no = (
+    SELECT dept_no
+    FROM departments
+    WHERE dept_name = 'Customer Service'
+  )
+);
+
+
+-- +-----------------+
+-- | dept_name       |
+-- +-----------------+
+-- | Development     |
+-- | Finance         |
+-- | Human Resources |
+-- | Research        |
+-- +-----------------+
+-- Find the first and last name of the employee with the highest salary.
+
+
+
+-- +------------+-----------+
+-- | first_name | last_name |
+-- +------------+-----------+
+-- | Tokuyasu   | Pesch     |
+-- +------------+-----------+
+
+
+
+--->  NOTES
 
 SELECT s.name, s.email, s.github_username
-FROM students s JOIN cohorts c
+FROM students s
+JOIN cohorts c
   ON s.cohort_id = c.id
 WHERE c.name = 'Ulysses';
 
 
 
 
-SELECT namem, email, github_username
+SELECT name, email, github_username
 WHERE chort__id = (
   SELECT id
   FROM chorts
@@ -21,37 +109,43 @@ WHERE chort__id = (
 SELECT
   CONCAT(e.last_name, ' ,', e.first_name)
   AS name
-FROM emplayees e
-JOIN dept_manager dm
-  ON dm.emp_no
+  FROM emplayees e
+  JOIN dept_manager dm
+  ON dm.emp_no = e.emp_no
+  JOIN departments d
+  ON dm.dept_no = d.dept_no
+  WHERE d.dept_name = 'Customer Service'
+    AND dm.to_date > now();
 
   --- HARD WAY
 
 
 SELECT *
-FROM dept_manger
-WHERE to_dater > curdate()
+  FROM dept_manager
+  WHERE to_date > curdate()
   AND dept_no = 'd009';
 
 SELECT *
-FROM departments
-WHERE dept_name = 'Coustmer Service';
+  FROM departments
+  WHERE dept_name = 'Coustmer Service';
 
 
 ---- EASY WAY
 
 SELECT first_name, last_name
-FROM employees
-WHERE emp_no =(
+  FROM employees
+  WHERE emp_no =(
   SELECT emp_no
   FROM dept_manager
-WHERE to_date > curdate()
-
-
+  WHERE to_date > curdate()
   AND dept_no = (
-
-SELECT
-
-)
-
+    SELECT dept_no
+    FROM departments
+    WHERE dept_name = 'Customer Service'
+  )
 );
+
+
+SELECT *
+  FROM employees
+  WHERE hire_date = '1990-10-22';
