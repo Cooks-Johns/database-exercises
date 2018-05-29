e7f96507afd77b3c336cf33efa0d5301e9c872b9
John <cooks.johns@gmail.com>
2018-05-28 19:41:20 -0500

    Employees database

--- join_exercises.sql
+++ join_exercises.sql
@@ -11,17 +11,27 @@ LIMIT 10;
 
 
 -- returns the names of
-
+-- 2
 
 SELECT CONCAT(em.first_name, ' ', em.last_name) AS full_name, d.dept_name
 FROM dept_manager as dmm
 JOIN employees as em
   ON em.emp_no = dmm.emp_no
 JOIN departments as d
-  ON d.dept_no = d.dept_no
-;
+ On d.dept_no = em.emp_no;
+
+-- 3
+
+-- 4
+
+-- 5
+
+
+-- BONUS
+
 
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+
 
 FROM employees as e
 LEFT JOIN dept_emp as de
